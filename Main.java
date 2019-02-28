1b5601bdebecd8dfc463c9459664c10aadd6b703
Денис Гриник <44347207+DenysHrynyk@users.noreply.github.com>
2019-02-28 09:58:05 +0200

    changed name

--- Main.java
+++ Main.java
@@ -1,19 +1,18 @@
 package com.company;
 
 public class Main {
-    public static void main(String[] args) {
+    public static  void main(String[]args) {
 
-        System.out.print(Hostel.work_day);
-        Hostel hostel_1 = new Hostel();
-        Hostel hostel_2 = new Hostel(1324, "Mountain", 22, "st.Shevchenko 46");
-        Hostel hostel_3 = new Hostel(125237, "Lviv", 1704, "st.Bandera 23",
+        System.out.print(Hostel.workDay);
+        Hostel hostelStar = new Hostel();
+        Hostel hostelMountain = new Hostel(1324, "Mountain", 22, "st.Shevchenko 46");
+        Hostel hostelLviv = new Hostel(125237, "Lviv", 1704, "st.Bandera 23",
                 4, 424, "middle");
 
-        System.out.print("\n" + hostel_1.toString());
-        System.out.print("\n" + hostel_2.toString());
-        System.out.print("\n" + hostel_3.toString());
+        System.out.print("\n" + hostelStar.toString());
+        System.out.print("\n" + hostelMountain.toString());
+        System.out.print("\n" + hostelLviv.toString());
 
-        Hostel.printStaticwork_day();
+        Hostel.printStaticWorkDay();
         }
     }
-
